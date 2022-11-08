5e3b3cf91bb76338557b27a26da3dc375524066b
Matt Jones <matt.jones@stfc.ac.uk>
2022-11-08 11:47:57 +0000

    added a super useful function

--- inflammation/models.py
+++ inflammation/models.py
@@ -35,3 +35,8 @@ def daily_max(data):
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
+
+def extra_function():
+    """ this prints hello world
+    """
+    print("Hello World")
